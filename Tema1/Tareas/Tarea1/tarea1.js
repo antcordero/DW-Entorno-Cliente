213a864220329ce121af0d47b59df2ad04355ee7
Antonio Cordero <antoniocorderomn@gmail.com>
2025-09-16 17:34:49 +0200

    16/05/25

--- Tema1/Tareas/Tarea1/tarea1.js
+++ Tema1/Tareas/Tarea1/tarea1.js
@@ -2,17 +2,15 @@ var btn = document.getElementById("btn");
 var foto = document.getElementById("foto");
 
 function cambiarFoto() {
-    if (img.src.match("1.jpg")) {
-        img.src = "../Tarea1/img/2.jpg";
-    } else if (img.src.match("2.jpg")) {
-        img.src = "../Tarea1/img/3.jpg";
-    } else if (img.src.match("3.jpg")) {
-        img.src = "../Tarea1/img/4.jpg";
-    } else if (img.src.match("3.jpg")) {
-        img.src = "../Tarea1/img/4.jpg";
-    } else if (img.src.match("4.jpg")) {
-        img.src = "../Tarea1/img/1.jpg";
-    } 
+    if (foto.src.match("img/1.jpg")) {
+        foto.src = "img/2.jpg";
+    } else if (foto.src.match("img/2.jpg")) {
+        foto.src = "img/3.jpg";
+    } else if (foto.src.match("img/3.jpg")) {
+        foto.src = "img/4.jpg";
+    } else if (foto.src.match("img/4.jpg")) {
+        foto.src = "img/1.jpg";
+    }
 }
 
 btn.addEventListener("click", cambiarFoto);
