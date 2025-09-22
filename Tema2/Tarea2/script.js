15fecfac39408108ee02e9ac7d7e7b899a8aff5a
Antonio Cordero <antoniocorderomn@gmail.com>
2025-09-22 13:37:41 +0200

    primeras actividades de clase en consola con JS

--- Tema2/Tarea2/script.js
+++ Tema2/Tarea2/script.js
@@ -7,4 +7,4 @@ var mensaje2 = prompt("¿Quieres abandonar la página? (ACEPTAR/CANCELAR)");
 
 console.log("El usuario " + nombre + " ha elegido: " + mensaje2);
 
-console.log("%cFIN DEL PROGRAMA", "color: blue; font-weight: bold;");
+console.log("%cFIN DEL PROGRAMA", "color: blue; font-weight: bold; text-decoration: underline;");
